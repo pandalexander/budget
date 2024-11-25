5ceb79dbd508275827e47de475bfd744198d9d45
Alex Seeley <alexseeley134@gmail.com>
2024-11-25 13:11:02 -0700

    Add reactive form for number of children

--- angular-budget/src/app/calculator/calculator.component.ts
+++ angular-budget/src/app/calculator/calculator.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-calculator',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.css',
 })
-export class CalculatorComponent {}
+export class CalculatorComponent {
+  numOfChildren = new FormControl(0);
+}
