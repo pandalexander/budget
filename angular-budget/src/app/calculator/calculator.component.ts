82da4e40305699a7c808dce7b564d226948184a7
Alex Seeley <alexseeley134@gmail.com>
2024-11-27 16:28:55 -0700

    Create form control for number of children

--- angular-budget/src/app/calculator/calculator.component.ts
+++ angular-budget/src/app/calculator/calculator.component.ts
@@ -1,72 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-
-import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.css',
 })
-export class CalculatorComponent implements OnInit {
-  currentYear = new Date().getFullYear();
+export class CalculatorComponent {
+  maxChildren: number = 10;
 
-  numOfChildren = new FormControl(1);
-  foo() {
-    console.log(this.numOfChildren.value);
+  childrenOptions = new Array(this.maxChildren).fill(0).map((_, i) => i + 1);
+
+  infoForm = new FormGroup({
+    numOfChildren: new FormControl('1'),
+  });
+
+  get numOfChildren() {
+    return this.infoForm.get('numOfChildren')!.value;
   }
 
-  form!: FormGroup;
-  numberOptions = [0, 1, 2, 3, 4, 5]; // Dropdown options for number of children
-
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit() {
-    // Initialize the form
-    this.form = this.fb.group({
-      numChildren: [0], // Initial dropdown value
-      birthYears: this.fb.array([]), // Dynamic fields for birth years
-    });
-
-    // Listen for changes to the number of children
-    this.form.get('numChildren')?.valueChanges.subscribe((num: number) => {
-      this.updateBirthYearFields(num);
-    });
-  }
-
-  // Getter for birthYears FormArray
-  get birthYears(): FormArray {
-    return this.form.get('birthYears') as FormArray;
-  }
-
-  // Update the FormArray based on the selected number of children
-  private updateBirthYearFields(num: number) {
-    const currentCount = this.birthYears.length;
-
-    if (num > currentCount) {
-      // Add controls if more children are selected
-      for (let i = currentCount; i < num; i++) {
-        this.birthYears.push(this.fb.group({ year: [''] }));
-      }
-    } else if (num < currentCount) {
-      // Remove controls if fewer children are selected
-      for (let i = currentCount - 1; i >= num; i--) {
-        this.birthYears.removeAt(i);
-      }
-    }
-  }
-
-  // Handle form submission
   onSubmit() {
-    if (this.form.invalid) {
-      // Mark all controls as touched to trigger validation messages
-      this.form.markAllAsTouched();
-      return;
-    }
-
-    console.log(this.form.value);
+    console.log(this.infoForm.value);
   }
 }
