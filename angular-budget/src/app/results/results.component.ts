652495ad1d031a0eb18e2887e216400884038e8f
Alex <alexseeley134@gmail.com>
2024-12-01 17:13:22 -0700

    Add getters to results component

--- angular-budget/src/app/results/results.component.ts
+++ angular-budget/src/app/results/results.component.ts
@@ -8,4 +8,32 @@ import { Component, Input } from '@angular/core';
 })
 export class ResultsComponent {
   @Input() payload: any = {};
+
+  get numberOfChildren(): number {
+    return this.payload.numberOfChildren;
+  }
+
+  get agesOfChildren() {
+    return this.payload.agesOfChildren;
+  }
+
+  get percentageOfFunding(): number {
+    return this.payload.percentageOfFunding;
+  }
+
+  get collegeCost(): number {
+    return this.payload.collegeCost;
+  }
+
+  get currentSavings(): number {
+    return this.payload.currentSavings;
+  }
+
+  get rateOfReturn(): number {
+    return this.payload.rateOfReturn;
+  }
+
+  get annualCollegeCostInflation(): number {
+    return this.payload.annualCollegeCostInflation;
+  }
 }
